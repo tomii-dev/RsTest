27a00b62dbbaf10f4228fb4b2a77f742a13561fd
tomii <tomii.dev0@gmail.com>
2022-07-07 17:08:11 +0100

    use const for better readability and move curly braces onto new line

--- src/object.cpp
+++ src/object.cpp
@@ -13,31 +13,38 @@ Object::Object(Scene* scene, glm::vec3 pos, glm::vec3 size)
       m_rotation    (1.0f)
 {}
 
-glm::vec3 Object::getPosition(){
+glm::vec3 Object::getPosition()
+{
     return m_position;
 }
 
-void Object::setPosition(glm::vec3 pos) {
+void Object::setPosition(glm::vec3 pos) 
+{
     m_position = pos;
 }
 
-float Object::getSize() {
+float Object::getSize() 
+{
     return m_size.x;
 }
 
-void Object::setSize(float size) {
+void Object::setSize(float size) 
+{
     m_size = glm::vec3(size);
 }
 
-void Object::update(){
-    unsigned int modelLoc = glGetUniformLocation(m_scene->getShader(), "u_Model");
+void Object::update()
+{
+    const unsigned int modelLoc = glGetUniformLocation(m_scene->getShader(), "u_Model");
     m_model = glm::translate(glm::mat4(1.0f), m_position)
         * m_rotation
         * glm::scale(glm::mat4(1.0f), m_size);
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &m_model[0][0]);
 }
+
 void Object::draw(){}
 
-void Object::rotate(float deg, glm::vec3 dir) {
+void Object::rotate(float deg, glm::vec3 dir) 
+{
     m_rotation = glm::rotate(m_rotation, glm::radians(deg), dir);
 }
