55a3e1d821e93d777016045d82f96a2d6aa0e947
tomii <hellotommybowles@gmail.com>
2022-08-08 16:22:23 +0100

    use cube maps for cube textures

--- src/object.cpp
+++ src/object.cpp
@@ -11,8 +11,7 @@ Object::Object(Scene* scene, glm::vec3 pos, glm::vec3 size)
     : m_scene       (scene),
       m_position    (pos),
       m_size        (size),
-      m_rotation    (1.0f),
-      m_texture     (0)
+      m_rotation    (1.0f)
 {}
 
 glm::vec3 Object::getPosition()
@@ -42,11 +41,14 @@ void Object::setRotation(float x, float y, float z)
 
 void Object::update(const ImageFrameData& imgData)
 {
-    const unsigned int modelLoc = glGetUniformLocation(m_scene->getShader(), "u_Model");
+    const GLint modelLoc = glGetUniformLocation(m_scene->getShader(), "u_Model");
     m_model = glm::translate(glm::mat4(1.0f), m_position)
         * m_rotation
         * glm::scale(glm::mat4(1.0f), m_size);
     glUniformMatrix4fv(modelLoc, 1, GL_FALSE, &m_model[0][0]);
+
+    const GLint typeLoc = glGetUniformLocation(m_scene->getShader(), "u_ObjectType");
+    glUniform1i(typeLoc, (int)m_type);
 
     // bind and set texture
     const GLint isTexLoc = glGetUniformLocation(m_scene->getShader(), "u_IsTextured");
@@ -63,22 +65,69 @@ void Object::update(const ImageFrameData& imgData)
     if (size == m_lastTexSize)
         return;
 
-    glGenTextures(1, &m_texture);
-    glBindTexture(GL_TEXTURE_2D, m_texture);
+    const GLint internalFormat  = utils::glInternalFormat(imgData.format);
+    const GLenum format         = utils::glFormat(imgData.format);
+    const GLenum type           = utils::glType(imgData.format);
+
+    Texture tmp;
+    glGenTextures(1, &tmp.id);
+    glBindTexture(GL_TEXTURE_2D, tmp.id);
+    tmp.target = GL_TEXTURE_2D;
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_REF_TO_TEXTURE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
-    glTexImage2D(GL_TEXTURE_2D, 0, utils::glInternalFormat(imgData.format), size.x, size.y,
-        0, utils::glFormat(imgData.format), utils::glType(imgData.format), nullptr);
+    glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, size.x, size.y, 0, format, type, nullptr);
+
     SenderFrameTypeData data;
-    data.gl.texture = m_texture;
+    data.gl.texture = tmp.id;
     if (utils::rsGetFrameImage(imgData.imageId, RS_FRAMETYPE_OPENGL_TEXTURE, data))
         utils::logToD3("failed to get texture param info");
     const GLint texLoc = glGetUniformLocation(m_scene->getShader(), "u_Texture");
     glUniform1i(texLoc, 0);
+
+    // Note: Could use polymorphism and perform texture setup specific to object in its own update
+    // overload, however after trying this I decided it would make things more complicated than they
+    // need to be and a simple switch in this method works fine
+    switch (m_type)
+    {
+    case Object_Cube:
+    {
+        // Texture needs to be 1:1 for cube map
+        const GLsizei texSize = size.x > size.y ? size.x : size.y;
+
+        // Get pixel data from texture provided by RS
+        GLubyte* pxData = new GLubyte[texSize * texSize * 4];
+        glGetTexImage(GL_TEXTURE_2D, 0, format, type, pxData);
+
+        // Unbind texture
+        glBindTexture(GL_TEXTURE_2D, 0);
+
+        // we want to use a cube map for texturing cube
+        glGenTextures(1, &m_texture.id);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, m_texture.id);
+        m_texture.target = GL_TEXTURE_CUBE_MAP;
+
+        for (uint32_t i = 0; i < 6; ++i)
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,
+                0, internalFormat, texSize, texSize, 0, format, type, pxData);
+
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+        glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+        break;
+    }
+    default:
+        m_texture = tmp;
+        break;
+    }
 
     m_lastTexSize = size;
 }
