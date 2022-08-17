0851038f760efd69dbceb57fa2aa6fe137cc8605
tomii <hellotommybowles@gmail.com>
2022-08-17 17:37:00 +0100

    remove redundant shape class, inherit directly from Object

--- include/shape.hpp
+++ include/shape.hpp
@@ -7,43 +7,21 @@
 #include <string>
 
 #include "object.hpp"
+#include "utils.hpp"
 
 #define WHITE glm::vec3(1,1,1)
 
-class Shape : public Object{
-protected:
-    unsigned int m_vao;
-    unsigned int m_vbo;
-    unsigned int m_ibo;
-    unsigned int m_cbo;
-    unsigned int m_nbo;
-    unsigned int m_tbo;
-    unsigned int m_shader;
-    std::vector<float> m_vertices;
-    std::vector<unsigned int> m_indices;
-    std::vector<float> m_normals;
-    std::vector<float> m_texCoords;
-    float m_size;
-    glm::vec3 m_colour;
-    virtual void init();
-public:
-    Shape(Scene* scene, glm::vec3 pos, float size, glm::vec3 colour, const std::string& name);
-    void update(const ImageFrameData& imgData) override;
-    void draw() override;
-};
-
-class Cube : public Shape{
-private:
-    void init() override;
+class Cube : public Object 
+{
 public:
     Cube(Scene* scene, glm::vec3 pos, float size, const std::string& name, glm::vec3 colour=WHITE);
 };
 
-class Sphere : public Shape {
+class Sphere : public Object 
+{
 private:
     int m_stackCount;
     int m_sectorCount;
-    void init() override;
 public:
     Sphere(Scene* scene, glm::vec3 pos, float size, const std::string& name, int stackCount, int sectorCount, glm::vec3 colour=WHITE);
     int getStacks();
--- src/shape.cpp
+++ src/shape.cpp
@@ -7,167 +7,86 @@
 #include <glm/gtx/vector_angle.hpp>
 #include "app.hpp"
 
-Shape::Shape(Scene* scene, glm::vec3 pos, float size, glm::vec3 colour, const std::string& name)
-    : Object		(scene, pos, glm::vec3(size), name),
-      m_size		(size),
-      m_colour		(colour)
-{}
-
-void Shape::init() {
-
-    const unsigned int shader = m_scene->getShader();
-
-    // generate vertex array
-    glGenVertexArrays(1, &m_vao);
-    glBindVertexArray(m_vao);
-
-    // generate vertex buffer
-    const unsigned int posLoc = glGetAttribLocation(shader, "a_Position");
-    glGenBuffers(1, &m_vbo);
-    glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*m_vertices.size(), m_vertices.data(), GL_DYNAMIC_DRAW);
-    glEnableVertexAttribArray(posLoc);
-    glVertexAttribPointer(posLoc, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-
-    // generate index buffer
-    glGenBuffers(1, &m_ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int)*m_indices.size(), m_indices.data(), GL_DYNAMIC_DRAW);
-
-    // generate normal buffer
-    const unsigned int normLoc = glGetAttribLocation(shader, "a_Normal");
-    glGenBuffers(1, &m_nbo);
-    glBindBuffer(GL_ARRAY_BUFFER, m_nbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float)*m_vertices.size(), m_normals.data(), GL_DYNAMIC_DRAW);
-    glEnableVertexAttribArray(normLoc);
-    glVertexAttribPointer(normLoc, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);
-
-    // generate tex coord buffer
-    const unsigned int texCoordLoc = glGetAttribLocation(shader, "a_TexCoord");
-    glGenBuffers(1, &m_tbo);
-    glBindBuffer(GL_ARRAY_BUFFER, m_tbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * m_texCoords.size(), m_texCoords.data(), GL_DYNAMIC_DRAW);
-    glEnableVertexAttribArray(texCoordLoc);
-    glVertexAttribPointer(texCoordLoc, 2, GL_FLOAT, GL_FALSE, sizeof(float)*2, 0);
-}
-
-void Shape::update(const ImageFrameData& imgData) {
-
-    glBindVertexArray(m_vao);
-
-    Object::update(imgData);
-
-    const unsigned int shader = m_scene->getShader();
-
-    // set colour uniform
-    const unsigned int colorLoc = glGetUniformLocation(shader, "u_ObjectColour");
-    glUniform3fv(colorLoc, 1, &m_colour[0]);
-}
-
-void Shape::draw() {
-    glBindTexture(m_texture.target, m_texture.id);
-    glDrawElements(GL_TRIANGLES, m_indices.size(), GL_UNSIGNED_INT, nullptr);
-    glBindTexture(m_texture.target, 0);
-}
-
 Cube::Cube(Scene* scene, glm::vec3 pos, float size, const std::string& name, glm::vec3 colour)
-    : Shape(scene, pos, size, colour, name)
+    : Object(scene, pos, glm::vec3(size), name)
 {
-    init();
-}
-
-void Cube::init() {
-
     m_type = Object_Cube;
+    
+    m_vao.addVertex(v3(-1,  1,  1 ), v2(1, 0), v3(0, 0, 1)); 
+    m_vao.addVertex(v3(-1,  1,  1 ), v2(0, 0), v3(0, 0, 1)); 
+    m_vao.addVertex(v3(-1,  1,  1 ), v2(0, 0), v3(0, 0, 1)); 
 
-    // temporary
-    // TODO: write algorithm to calculate vertices from center point
-    std::vector<float> vertices = {
-        -1.0f,1.0f,1.0f,
-        -1.0f,-1.0f,1.0f,
-        1.0f,1.0f,1.0f,
-        1.0f,-1.0f,1.0f,
-        -1.0f,1.0f,-1.0f,
-        -1.0f,-1.0f,-1.0f,
-        1.0f,1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-    };
+    m_vao.addVertex(v3(-1, -1,  1 ), v2(1, 1), v3(0, 0, 1)); 
+    m_vao.addVertex(v3(-1, -1,  1 ), v2(0, 1), v3(0, 0, 1)); 
+    m_vao.addVertex(v3(-1, -1,  1 ), v2(0, 1), v3(0, 0, 1)); 
 
-    m_vertices = vertices;
+    m_vao.addVertex(v3( 1,  1,  1 ), v2(0, 0), v3(1, 0, 0)); 
+    m_vao.addVertex(v3( 1,  1,  1 ), v2(1, 0), v3(1, 0, 0)); 
+    m_vao.addVertex(v3( 1,  1,  1 ), v2(1, 0), v3(1, 0, 0)); 
 
-    std::vector<unsigned int> indices = {
-        0, 2, 3,
-        0, 3, 1,
-        0, 1, 5,
-        0, 4, 5,
-        0, 2, 6,
-        0, 4, 6,
-        4, 5, 7,
-        4, 6, 7,
-        3, 2, 6,
-        3, 7, 6,
-        3, 1, 5,
-        3, 7, 5
-    };
+    m_vao.addVertex(v3( 1,  -1,  1), v2(0, 1), v3(1, 0, 0)); 
+    m_vao.addVertex(v3( 1,  -1,  1), v2(1, 1), v3(1, 0, 0)); 
+    m_vao.addVertex(v3( 1,  -1,  1), v2(1, 1), v3(1, 0, 0)); 
 
-    m_indices = indices;
+    m_vao.addVertex(v3(-1,  1, -1), v2(1, 0), v3(-1, 0, 0)); 
+    m_vao.addVertex(v3(-1,  1, -1), v2(0, 1), v3(-1, 0, 0)); 
+    m_vao.addVertex(v3(-1,  1, -1), v2(0, 0), v3(-1, 0, 0)); 
 
-    std::vector<float> normals = {
-        0, 0, 1,
-        0, 0, 1,
-        1, 0, 0,
-        1, 0, 0,
-        -1, 0, 0,
-        0, 0, -1,
-        0, 1, 0,
-        0, 0, -1
-    };
+    m_vao.addVertex(v3(-1, -1, -1), v2(1, 1), v3(0, 0, -1)); 
+    m_vao.addVertex(v3(-1, -1, -1), v2(0, 1), v3(0, 0, -1)); 
+    m_vao.addVertex(v3(-1, -1, -1), v2(0, 0), v3(0, 0, -1)); 
 
-    m_normals = normals;
+    m_vao.addVertex(v3( 1,  1, -1 ), v2(1, 1), v3(0, 1, 0));
+    m_vao.addVertex(v3( 1,  1, -1 ), v2(1, 0), v3(0, 1, 0)); 
+    m_vao.addVertex(v3( 1,  1, -1 ), v2(0, 0), v3(0, 1, 0)); 
 
-    m_texCoords.resize(16);
+    m_vao.addVertex(v3( 1, -1, -1), v2(1, 1), v3(0, 0, -1)); 
+    m_vao.addVertex(v3( 1, -1, -1), v2(0, 1), v3(0, 0, -1)); 
+    m_vao.addVertex(v3( 1, -1, -1), v2(1, 0), v3(0, 0, -1)); 
 
-    Shape::init();
+    m_vao.setIndices({
+        0, 6, 9, 0, 9, 3,
+        1, 4, 15, 1, 12, 15,
+        2, 7, 18, 2, 13, 18,
+        14, 16, 21, 14, 19, 21,
+        10, 8, 20, 10, 22, 20,
+        11, 5, 17, 11, 23, 17
+    });
+
+    m_vao.build();
 }
 
 Sphere::Sphere(Scene* scene, glm::vec3 pos, float radius, const std::string& name, int stackCount, int sectorCount, glm::vec3 colour)
-    : Shape				(scene, pos, radius * 2.f, colour, name),
+    : Object		    (scene, pos, glm::vec3(radius * 2.f), name),
       m_stackCount		(stackCount),
       m_sectorCount		(sectorCount)
 {
-    init();
-}
-
-void Sphere::init() {
-
     m_type = Object_Sphere;
 
     int stackIt = 0;
     int	secIt = 0;
     float stackStep = PI / m_stackCount;
     float sectorStep = 2 * PI / m_sectorCount;
-    
-    while (stackIt <= m_stackCount) {
 
+    while (stackIt <= m_stackCount) {
         float stackAngle = PI / 2 - stackIt * stackStep;
         float sectorAngle = secIt * sectorStep;
 
-        glm::vec3 pos(
+        v3 pos(
             cosf(stackAngle) * cosf(sectorAngle),
             cosf(stackAngle) * sinf(sectorAngle),
             sinf(stackAngle)
         );
 
-        for (int i = 0; i < 3; ++i) {
-            m_vertices.push_back(pos[i]);
-            m_normals.push_back(pos[i]);
-        }
+        v3 normal(pos);
 
         // tex coords
-        float s = (float)secIt / m_sectorCount;
-        float t = (float)stackIt / m_stackCount;
-        m_texCoords.push_back(s);
-        m_texCoords.push_back(t);
+        v2 texCoord(
+            (float)secIt / m_sectorCount,
+            (float)stackIt / m_stackCount
+        );
+
+        m_vao.addVertex(pos, texCoord, normal);
 
         if (secIt == m_sectorCount) {
             secIt = 0;
@@ -184,26 +103,26 @@ void Sphere::init() {
         int stackBegin = stackIt * (m_sectorCount + 1);
         int nextStack = stackBegin + m_sectorCount + 1;
 
-        for (int j = 0; j < m_sectorCount; ++j, ++stackBegin, ++nextStack) 
+        for (int j = 0; j < m_sectorCount; ++j, ++stackBegin, ++nextStack)
         {
             if (stackIt)
             {
-                m_indices.push_back(stackBegin);
-                m_indices.push_back(nextStack);
-                m_indices.push_back(stackBegin + 1);
+                m_vao.addIndex(stackBegin);
+                m_vao.addIndex(nextStack);
+                m_vao.addIndex(stackBegin + 1);
             }
 
             if (stackIt != m_stackCount - 1)
             {
-                m_indices.push_back(stackBegin + 1);
-                m_indices.push_back(nextStack);
-                m_indices.push_back(nextStack + 1);
+                m_vao.addIndex(stackBegin + 1);
+                m_vao.addIndex(nextStack);
+                m_vao.addIndex(nextStack + 1);
             }
         }
         stackIt++;
     }
 
-    Shape::init();
+    m_vao.build();
 }
 
 int Sphere::getStacks() {
@@ -212,7 +131,6 @@ int Sphere::getStacks() {
 
 void Sphere::setStacks(int count) {
     m_stackCount = count;
-    init();
 }
 
 int Sphere::getSectors() {
@@ -220,6 +138,4 @@ int Sphere::getSectors() {
 }
 
 void Sphere::setSectors(int count) {
-    m_sectorCount = count;
-    init();
 }
