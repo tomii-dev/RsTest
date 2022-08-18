fea26c4ffdc4a013bc903abadf17d7e05f4e4f75
tomii <hellotommybowles@gmail.com>
2022-08-18 10:55:50 +0100

    new variable naming conventions in glsl
    - new ambient colour uniform to determine colour of ambient light
    - change remote parameter naming

--- src/scene.cpp
+++ src/scene.cpp
@@ -16,23 +16,23 @@ Scene::Scene(const char* name) : m_currentCamera(new Camera(this, glm::vec3(-10,
                                  m_light        (glm::vec3(20.f, -15.f, 0.f), 1.f, .4f, VEC1)
 {
     const GLchar* vsSource[] = {R"src(#version 330 core
-    layout (location = 0) in vec4 a_Position;
-    layout (location = 1) in vec2 a_TexCoord;
-    layout (location = 2) in vec4 a_Normal;
+    layout (location = 0) in vec4 aPosition;
+    layout (location = 1) in vec2 aTexCoord;
+    layout (location = 2) in vec4 aNormal;
 
     out vec4 fragPos;
     out vec4 normal;
     out vec2 texCoord;
 
-    uniform mat4 u_Model;
-    uniform mat4 u_View;
-    uniform mat4 u_Proj;
+    uniform mat4 uModel;
+    uniform mat4 uView;
+    uniform mat4 uProj;
 
     void main() {
-        fragPos = u_Model * a_Position;
-        normal = u_Model * a_Normal;
-        texCoord = vec2(1, 1) - a_TexCoord;
-        gl_Position = u_Proj * u_View * fragPos;
+        fragPos = uModel * aPosition;
+        normal = uModel * aNormal;
+        texCoord = vec2(1, 1) - aTexCoord;
+        gl_Position = uProj * uView * fragPos;
     }
     )src" };
 
@@ -41,25 +41,25 @@ Scene::Scene(const char* name) : m_currentCamera(new Camera(this, glm::vec3(-10,
     in vec4 normal;
     in vec2 texCoord;
 
-    uniform vec3 u_LightPos;
-    uniform vec3 u_LightColour;
-    uniform float u_LightBrightness;
-    uniform float u_AmbientStrength;
-    uniform bool u_IsTextured;
-    uniform sampler2D u_Texture;
-    uniform samplerCube u_CubeMap;
+    uniform vec3 uLightPos;
+    uniform vec3 uLightColour;
+    uniform float uLightBrightness;
+    uniform float uAmbientColour;
+    uniform float uAmbientStrength;
+    uniform bool uIsTextured;
+    uniform sampler2D uTexture;
 
     void main(){
-        vec3 ambient = u_AmbientStrength * u_LightColour;
+        vec3 ambient = uAmbientStrength * uAmbientColour;
         vec4 norm = normalize(normal);
         vec4 texColour;
-        if (u_IsTextured)
-            texColour = texture(u_Texture, texCoord);
+        if (uIsTextured)
+            texColour = texture(uTexture, texCoord);
         else
             texColour = vec4(1, 1, 1, 1);
-        vec4 lightDir = normalize(vec4(u_LightPos, 1.f) - fragPos);
-        float diffuseStrength = max(dot(norm, lightDir), 0.0f) * u_LightBrightness;	
-        vec3 diffuse = diffuseStrength * u_LightColour;
+        vec4 lightDir = normalize(vec4(uLightPos, 1.f) - fragPos);
+        float diffuseStrength = max(dot(norm, lightDir), 0.0f) * uLightBrightness;	
+        vec3 diffuse = diffuseStrength * uLightColour;
         vec3 result = ambient + diffuse;
         gl_FragColor = vec4(result, 1.0f) * texColour;
     }
@@ -73,14 +73,14 @@ Scene::Scene(const char* name) : m_currentCamera(new Camera(this, glm::vec3(-10,
     m_rsScene->name = name;
 
     // Parameters for scene light
-    m_rsScene->addParam(RsFloatParam("lightpos_x", "posX", "light", 0, -100, 100, 0.1));
-    m_rsScene->addParam(RsFloatParam("lightpos_y", "posY", "light", 5, -100, 100, 0.1));
-    m_rsScene->addParam(RsFloatParam("lightpos_z", "posZ", "light", 0, -100, 100, 0.1));
+    m_rsScene->addParam(RsFloatParam("lightpos_x", "pos_x", "light", 0, -100, 100, 0.1));
+    m_rsScene->addParam(RsFloatParam("lightpos_y", "pos_y", "light", 5, -100, 100, 0.1));
+    m_rsScene->addParam(RsFloatParam("lightpos_z", "pos_z", "light", 0, -100, 100, 0.1));
     m_rsScene->addParam(RsFloatParam("lightcol_r", "light_r", "light", 1, 0, 1, .01));
     m_rsScene->addParam(RsFloatParam("lightcol_g", "light_g", "light", 1, 0, 1, .01));
     m_rsScene->addParam(RsFloatParam("lightcol_b", "light_b", "light", 1, 0, 1, .01));
     m_rsScene->addParam(RsFloatParam("lightcol_a", "light_a", "light", 1, 0, 1, .01));
-    m_rsScene->addParam(RsFloatParam("amb_strength", "ambient strength", "light", .4, 0, .5, 0.05));
+    m_rsScene->addParam(RsFloatParam("amb_strength", "ambient strength", "light", .4, 0, 1, 0.05));
     m_rsScene->addParam(RsFloatParam("brightness", "brightness", "light", 1, 0, 2, 0.1));
 
     App::getSchema().addScene(*m_rsScene);
@@ -103,8 +103,8 @@ void Scene::updateMatrices() {
     const glm::mat4 m_projection = glm::perspective(glm::radians(m_currentCamera->getFov()), width / height, 0.1f, 9000.0f);
     const glm::mat4 m_view = glm::lookAt(camPos, camPos + camFront, camUp);
 
-    const unsigned int viewLoc = glGetUniformLocation(m_shader, "u_View");
-    const unsigned int projLoc = glGetUniformLocation(m_shader, "u_Proj");
+    const unsigned int viewLoc = glGetUniformLocation(m_shader, "uView");
+    const unsigned int projLoc = glGetUniformLocation(m_shader, "uProj");
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &m_view[0][0]);
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, &m_projection[0][0]);
 }
@@ -121,10 +121,10 @@ void Scene::render(){
     m_light.setAmbientStrength(params[7]);
     m_light.setBrightness(params[8]);
 
-    const unsigned int lightPosLoc = glGetUniformLocation(m_shader, "u_LightPos");
-    const unsigned int lightColourLoc = glGetUniformLocation(m_shader, "u_LightColour");
-    const unsigned int brightnessLoc = glGetUniformLocation(m_shader, "u_LightBrightness");
-    const unsigned int ambientLoc = glGetUniformLocation(m_shader, "u_AmbientStrength");
+    const unsigned int lightPosLoc = glGetUniformLocation(m_shader, "uLightPos");
+    const unsigned int lightColourLoc = glGetUniformLocation(m_shader, "uLightColour");
+    const unsigned int brightnessLoc = glGetUniformLocation(m_shader, "uLightBrightness");
+    const unsigned int ambientLoc = glGetUniformLocation(m_shader, "uAmbientStrength");
 
     glUniform3fv(lightPosLoc, 1, &m_light.getPosition()[0]);
     glUniform3fv(lightColourLoc, 1, &m_light.getColour()[0]);
@@ -173,12 +173,12 @@ Object* Scene::addObject(ObjectType type, ObjectArgs args){
 
     m_objects.push_back(obj);
 
-    m_rsScene->addParam(RsFloatParam(args.name + "pos_x", "posX", args.name, args.pos.x, -100, 100, 0.1));
-    m_rsScene->addParam(RsFloatParam(args.name + "pos_y", "posY", args.name, args.pos.y, -100, 100, 0.1));
-    m_rsScene->addParam(RsFloatParam(args.name + "pos_z", "posZ", args.name, args.pos.z, -100, 100, 0.1));
-    m_rsScene->addParam(RsFloatParam(args.name + "rot_x", "rotX", args.name, 0, 0, 359, 1));
-    m_rsScene->addParam(RsFloatParam(args.name + "rot_y", "rotY", args.name, 0, 0, 359, 1));
-    m_rsScene->addParam(RsFloatParam(args.name + "rot_z", "rotZ", args.name, 0, 0, 359, 1));
+    m_rsScene->addParam(RsFloatParam(args.name + "pos_x", "pos_x", args.name, args.pos.x, -100, 100, 0.1));
+    m_rsScene->addParam(RsFloatParam(args.name + "pos_y", "pos_y", args.name, args.pos.y, -100, 100, 0.1));
+    m_rsScene->addParam(RsFloatParam(args.name + "pos_z", "pos_z", args.name, args.pos.z, -100, 100, 0.1));
+    m_rsScene->addParam(RsFloatParam(args.name + "rot_x", "rot_x", args.name, 0, 0, 359, 1));
+    m_rsScene->addParam(RsFloatParam(args.name + "rot_y", "rot_y", args.name, 0, 0, 359, 1));
+    m_rsScene->addParam(RsFloatParam(args.name + "rot_z", "rot_z", args.name, 0, 0, 359, 1));
 
     m_rsScene->addParam(RsTextureParam(args.name + "texture", "texture", args.name));
 
