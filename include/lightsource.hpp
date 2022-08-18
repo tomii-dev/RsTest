6dd499252d34378ab9bb4f36f59fb19b804c8fee
tomii <hellotommybowles@gmail.com>
2022-08-18 14:19:25 +0100

    use vec4 for colour in light and move ambient information to scene

--- include/lightsource.hpp
+++ include/lightsource.hpp
@@ -1,25 +1,23 @@
 #pragma once
 
 #include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 
 class Object;
 
 class LightSource{
 private:
     float m_brightness;
-    float m_ambientStrength;
     glm::vec3 m_position;
-    glm::vec3 m_colour;
+    glm::vec4 m_colour;
     Object* m_obj;
 public:
-    LightSource(glm::vec3 position, float brightness=0.0f, float ambientStrength=0.0f, glm::vec3 colour=glm::vec3(), Object* obj=nullptr);
+    LightSource(glm::vec3 position, float brightness=0.0f, float ambientStrength=0.0f, glm::vec4 colour=glm::vec4(), Object* obj=nullptr);
     void render();
     float getBrightness();
-    float getAmbientStrength();
-    void setAmbientStrength(float strength);
     void setBrightness(float brightness);
     glm::vec3 getPosition();
     void setPosition(glm::vec3 position);
-    glm::vec3 getColour();
-    void setColour(glm::vec3 colour);
+    glm::vec4 getColour();
+    void setColour(glm::vec4 colour);
 };
